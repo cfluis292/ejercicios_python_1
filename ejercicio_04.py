300fbedb30fdb49746338b330ae2b673993a96a0
Luis Cisneros <cfluis292@gmail.com>
2026-01-23 10:37:32 -0600

    Mi segundo commit

---
+++ ejercicio_04.py
@@ -0,0 +1,12 @@
+num_1 = int(input("Ingresa un numero: "))
+num_2 = int(input("Ingresa otro numero: "))
+
+suma = num_1 + num_2
+resta = num_1 - num_2
+multi = num_1 * num_2
+divi = num_1 / num_2
+
+print("Suma: ", suma)
+print("Resta: ", resta)
+print("Multiplicacion: ", multi)
+print("Division: ", divi)
---
+++ ejercicio_05.py
@@ -0,0 +1,8 @@
+
+
+farenheit = float(input("Ingresa los grados Farenheit: "))
+
+celsius = (farenheit - 32) * (5 / 9)
+
+print("Farenheit: ", farenheit)
+print("Celcius: ", celcius)
